6fc97c31fbc3483076f09a0ef680ca3eb9e33885
Arpit-0525 <sarpit0203@gmail.com>
2025-03-10 10:53:16 +0530

    added some data

--- student registration/main.py
+++ student registration/main.py
@@ -2,21 +2,21 @@ from tkinter import *
 from datetime import date
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image ,ImageTk
+from PIL import Image, ImageTk
 import os
 from tkinter.ttk import Combobox
-import openpyxl , xlrd
+import openpyxl, xlrd
 from openpyxl import Workbook
 import pathlib
 
-background = "#06283D"
-framebg = "#EDEDED"
-framefg = "#06283D"
+BACKGROUND = "#06283D"
+FRAME_BG = "#EDEDED"
+FRAME_FG = "#06283D"
 
 root = Tk()
 root.title("STUDENT REGISTRATION SYSTEM ")
 root.geometry("1250x700+210+100")
-root.config(bg = background  )
+root.config(bg=BACKGROUND)
 
 file = pathlib.Path('Student_data.xlsx')
 if file.exists():
@@ -45,32 +45,35 @@ else:
 def Exit():
     root.destroy()
 
+
 # Show image
 def showimage():
     global filename
     global img
-    filename= filedialog.askopenfilename(initialdir = os.getcwd(),
-                                         title="Select Image file", filetype=(("JPG File","*.jpg"),
-                                                                              ("PNG File","*.png"),
-                                                                              ("All File","*.txt")))
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                          title="Select Image file", filetype=(("JPG File", "*.jpg"),
+                                                                               ("PNG File", "*.png"),
+                                                                               ("All File", "*.txt")))
     img = (Image.open(filename))
-    resized_img = img.resize((190,190))
+    resized_img = img.resize((190, 190))
     photo2 = ImageTk.PhotoImage(resized_img)
     lbl.config(image=photo2)
     lbl.image = photo2
 
+
 # Registration no.
+
 
 def registration_no():
     file = openpyxl.load_workbook('Student_data.xlsx')
     sheet = file.active
     row = sheet.max_row
-    max_row_value = sheet.cell(row=row , column =1).value
+    max_row_value = sheet.cell(row=row, column=1).value
     try:
-        Registration.set(max_row_value+1)
-
+        Registration.set(max_row_value + 1)
     except:
         Registration.set("1")
+
 
 # Clear
 def Clear():
@@ -91,17 +94,18 @@ def Clear():
     lbl.config(image=img1)
     lbl.image = img1
 
-    img=""
+    img = ""
 
-# Save 
+
+# Save
 def Save():
     R1 = Registration.get()
     N1 = Name.get()
     C1 = Class.get()
     try:
-        G1=gender
+        G1 = gender
     except:
-        messagebox.showerror("ERROR!" , "Select Gender !")
+        messagebox.showerror("ERROR!", "Select Gender !")
     D2 = DOB.get()
     D1 = Date.get()
     Re1 = Religion.get()
@@ -111,36 +115,37 @@ def Save():
     F1 = Father_Occupation.get()
     M1 = Mother_Occupation.get()
 
-    if N1=="" or C1=="Select Class" or D2=="" or Re1== "" or S1=="" or  fathername=="" or  mothername=="" or  F1=="" or  M1=="":
-        messagebox.showerror("ERROR!" , "Few Data is Missing !")
+    if N1 == "" or C1 == "Select Class" or D2 == "" or Re1 == "" or S1 == "" or fathername == "" or mothername == "" or F1 == "" or M1 == "":
+        messagebox.showerror("ERROR!", "Few Data is Missing !")
 
     else:
-        file= openpyxl.load_workbook('Student_data.xlsx')
+        file = openpyxl.load_workbook('Student_data.xlsx')
         sheet = file.active
-        sheet.cell(column=1 , row = sheet.max_row+1 , value= R1)
-        sheet.cell(column=2 , row = sheet.max_row , value= N1)
-        sheet.cell(column=3 , row = sheet.max_row , value= C1)
-        sheet.cell(column=4 , row = sheet.max_row , value= G1)
-        sheet.cell(column=5 , row = sheet.max_row , value= D2)
-        sheet.cell(column=6 , row = sheet.max_row , value= D1)
-        sheet.cell(column=7 , row = sheet.max_row , value= Re1)
-        sheet.cell(column=8 , row = sheet.max_row , value= S1)
-        sheet.cell(column=9 , row = sheet.max_row , value= fathername)
-        sheet.cell(column=10 , row = sheet.max_row , value= mothername)
-        sheet.cell(column=11 , row = sheet.max_row , value= F1)
-        sheet.cell(column=12 , row = sheet.max_row , value= M1)
+        sheet.cell(column=1, row=sheet.max_row + 1, value=R1)
+        sheet.cell(column=2, row=sheet.max_row, value=N1)
+        sheet.cell(column=3, row=sheet.max_row, value=C1)
+        sheet.cell(column=4, row=sheet.max_row, value=G1)
+        sheet.cell(column=5, row=sheet.max_row, value=D2)
+        sheet.cell(column=6, row=sheet.max_row, value=D1)
+        sheet.cell(column=7, row=sheet.max_row, value=Re1)
+        sheet.cell(column=8, row=sheet.max_row, value=S1)
+        sheet.cell(column=9, row=sheet.max_row, value=fathername)
+        sheet.cell(column=10, row=sheet.max_row, value=mothername)
+        sheet.cell(column=11, row=sheet.max_row, value=F1)
+        sheet.cell(column=12, row=sheet.max_row, value=M1)
 
         file.save(r'Student_data.xlsx')
         try:
-            img.save("Student Images/"+ str(R1)+".jpg")
+            img.save("Student Images/" + str(R1) + ".jpg")
 
         except:
-            messagebox.showinfo("INFO" , "Profile Picture is not Available !!! ")
-        messagebox.showinfo("INFO" , "Data entered Sucessfully !!! ")
+            messagebox.showinfo("INFO", "Profile Picture is not Available !!! ")
+        messagebox.showinfo("INFO", "Data entered Sucessfully !!! ")
 
         Clear()
-        
+
         registration_no()
+
 
 # Search
 def search():
@@ -156,8 +161,8 @@ def search():
         if row[0].value == int(text):
             name = row[0]
             # print(str(name))
-            reg_no_position =str(name)[15:-1]
-            reg_number =str(name)[16:-1]
+            reg_no_position = str(name)[15:-1]
+            reg_number = str(name)[16:-1]
             # print(reg_no_position)
             # print(reg_number)
 
@@ -165,30 +170,29 @@ def search():
         print(str(name))
 
     except:
-        messagebox.showerror("INVALID!!" , "Invalid Registration Number !!")
+        messagebox.showerror("INVALID!!", "Invalid Registration Number !!")
 
-    x1 = sheet.cell(row=int(reg_number), column = 1).value
-    x2 = sheet.cell(row=int(reg_number), column = 2).value
-    x3 = sheet.cell(row=int(reg_number), column = 3).value
-    x4 = sheet.cell(row=int(reg_number), column = 4).value
-    x5 = sheet.cell(row=int(reg_number), column = 5).value
-    x6 = sheet.cell(row=int(reg_number), column = 6).value
-    x7 = sheet.cell(row=int(reg_number), column = 7).value
-    x8 = sheet.cell(row=int(reg_number), column = 8).value
-    x9 = sheet.cell(row=int(reg_number), column = 9).value
-    x10 = sheet.cell(row=int(reg_number), column = 10).value
-    x11 = sheet.cell(row=int(reg_number), column = 11).value
-    x12 = sheet.cell(row=int(reg_number), column = 12).value
+    x1 = sheet.cell(row=int(reg_number), column=1).value
+    x2 = sheet.cell(row=int(reg_number), column=2).value
+    x3 = sheet.cell(row=int(reg_number), column=3).value
+    x4 = sheet.cell(row=int(reg_number), column=4).value
+    x5 = sheet.cell(row=int(reg_number), column=5).value
+    x6 = sheet.cell(row=int(reg_number), column=6).value
+    x7 = sheet.cell(row=int(reg_number), column=7).value
+    x8 = sheet.cell(row=int(reg_number), column=8).value
+    x9 = sheet.cell(row=int(reg_number), column=9).value
+    x10 = sheet.cell(row=int(reg_number), column=10).value
+    x11 = sheet.cell(row=int(reg_number), column=11).value
+    x12 = sheet.cell(row=int(reg_number), column=12).value
 
     Registration.set(x1)
     Name.set(x2)
     Class.set(x3)
-    
-    if x4 =='Female':
+
+    if x4 == 'Female':
         R2.select()
     else:
         R1.select()
-    
 
     DOB.set(x5)
     Date.set(x6)
@@ -200,9 +204,9 @@ def search():
     Mother_Occupation.set(x12)
 
     img = (Image.open("Student Images/" + str(x1) + ".jpg"))
-    resized_image = img.resize((190,190))
+    resized_image = img.resize((190, 190))
     photo2 = ImageTk.PhotoImage(resized_image)
-    lbl.config(image= photo2)
+    lbl.config(image=photo2)
     lbl.image = photo2
 
 
@@ -223,7 +227,6 @@ def Update():
     F1 = Father_Occupation.get()
     M1 = Mother_Occupation.get()
 
-
     file = openpyxl.load_workbook('Student_data.xlsx')
     sheet = file.active
 
@@ -231,21 +234,21 @@ def Update():
         if row[0].value == R1:
             name = row[0]
             # print(str(name))
-            reg_no_position =str(name)[15:-1]
-            reg_number =str(name)[16:-1]
+            reg_no_position = str(name)[15:-1]
+            reg_number = str(name)[16:-1]
 
     # sheet.cell(column = 1 , row= int(reg_number),value=R1)
-    sheet.cell(column = 2 , row= int(reg_number),value=N1)
-    sheet.cell(column = 3 , row= int(reg_number),value=C1)
-    sheet.cell(column = 4 , row= int(reg_number),value=G1)
-    sheet.cell(column = 5 , row= int(reg_number),value=D2)
-    sheet.cell(column = 6 , row= int(reg_number),value=D1)
-    sheet.cell(column = 7 , row= int(reg_number),value=Re1)
-    sheet.cell(column = 8 , row= int(reg_number),value=S1)
-    sheet.cell(column = 9 , row= int(reg_number),value=fathername)
-    sheet.cell(column = 10 , row= int(reg_number),value=mothername)
-    sheet.cell(column = 11 , row= int(reg_number),value=F1)
-    sheet.cell(column = 12 , row= int(reg_number),value=M1)
+    sheet.cell(column=2, row=int(reg_number), value=N1)
+    sheet.cell(column=3, row=int(reg_number), value=C1)
+    sheet.cell(column=4, row=int(reg_number), value=G1)
+    sheet.cell(column=5, row=int(reg_number), value=D2)
+    sheet.cell(column=6, row=int(reg_number), value=D1)
+    sheet.cell(column=7, row=int(reg_number), value=Re1)
+    sheet.cell(column=8, row=int(reg_number), value=S1)
+    sheet.cell(column=9, row=int(reg_number), value=fathername)
+    sheet.cell(column=10, row=int(reg_number), value=mothername)
+    sheet.cell(column=11, row=int(reg_number), value=F1)
+    sheet.cell(column=12, row=int(reg_number), value=M1)
 
     file.save(r'Student_data.xlsx')
 
@@ -259,142 +262,130 @@ def Update():
     Clear()
 
 
-
-
-
 #gender
 def selection():
     global gender
     value = radio.get()
     if value == 1:
         gender = "Male"
-    else :
+    else:
         gender = "Female"
 
 
-
-
 # top frames
-Label(root, text= "Email: ", width=10 ,height=3 ,bg="#f0687c" ,anchor='e').pack(side=TOP ,fill=X)
-Label(root, text= "STUDENT REGISTRATION ", width=10 ,height=2 ,bg="#c36464" ,fg="#fff" ,font="arial 20 bold").pack(side=TOP ,fill=X)
+Label(root, text="Email: abc123xyz@gmail.com ", width=10, height=3, bg="#f0687c", anchor='e').pack(side=TOP, fill=X)
+Label(root, text="STUDENT REGISTRATION ", width=10, height=2, bg="#c36464", fg="#fff", font="arial 20 bold").pack(
+    side=TOP, fill=X)
 
 # searchbox to update
 Search = StringVar()
-Entry(root ,textvariable = Search ,width=15 , bd = 2 , font = "arial 20").place(x = 820 , y = 70)
-srch = Button(root , text = "Search" , compound=LEFT  , width=15 , bg="#68ddfa" , font = "arial 13 bold" , command= search )
-srch.place(x = 1060 , y = 66)
+Entry(root, textvariable=Search, width=15, bd=2, font="arial 20").place(x=820, y=70)
+srch = Button(root, text="Search", compound=LEFT, width=15, bg="#68ddfa", font="arial 13 bold", command=search)
+srch.place(x=1060, y=66)
 
-Update_Button = Button(root , text= "Update" , bg= "#68ddfa" , command=Update)
-Update_Button.place(x = 110 , y = 64)
+Update_Button = Button(root, text="Update", bg="#68ddfa", command=Update)
+Update_Button.place(x=110, y=64)
 
 # Registration and date
-Label(root , text= "Registration No. :" , font= "arial 13" , fg= framebg , bg= background ).place(x = 30 , y = 150)
-Label(root , text= "Date :" , font= "arial 13" , fg= framebg , bg= background ).place(x =500 , y = 150)
+Label(root, text="Registration No. :", font="arial 13", fg=FRAME_BG, bg=BACKGROUND).place(x=30, y=150)
+Label(root, text="Date :", font="arial 13", fg=FRAME_BG, bg=BACKGROUND).place(x=500, y=150)
 
 Registration = IntVar()
 Date = StringVar()
 
-reg_entry = Entry(root , textvariable= Registration , width= 15 , font="arial 10")
-reg_entry.place(x= 160 , y= 150)
+reg_entry = Entry(root, textvariable=Registration, width=15, font="arial 10")
+reg_entry.place(x=160, y=150)
 registration_no()
-
 
 today = date.today()
 d1 = today.strftime("%d/%m/%Y")
-date_entry = Entry(root, textvariable= Date, width= 15 , font="arial 10")
-date_entry.place(x= 550 , y= 150)
+date_entry = Entry(root, textvariable=Date, width=15, font="arial 10")
+date_entry.place(x=550, y=150)
 
 Date.set(d1)
 
 # Student Details
-obj = LabelFrame(root , text= "Student's Detail :" ,font = 20 , bd=2 , width= 900 , bg= framebg , fg= framefg, height=250 , relief=GROOVE )
-obj.place(x=30 , y= 200)
+obj = LabelFrame(root, text="Student's Detail :", font=20, bd=2, width=900, bg=FRAME_BG, fg=FRAME_FG, height=250,
+                 relief=GROOVE)
+obj.place(x=30, y=200)
 
-Label(obj, text= "Full Name :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=30, y=50)
-Label(obj, text= "Date of Birth :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=30, y=100)
-Label(obj, text= "Gender :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=30, y=150)
+Label(obj, text="Full Name :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=30, y=50)
+Label(obj, text="Date of Birth :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=30, y=100)
+Label(obj, text="Gender :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=30, y=150)
 
-Label(obj, text= "Class :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=500, y=50)
-Label(obj, text= "Religion :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=500, y=100)
-Label(obj, text= "Skill :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=500, y=150)
+Label(obj, text="Class :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=500, y=50)
+Label(obj, text="Religion :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=500, y=100)
+Label(obj, text="Skill :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=500, y=150)
 
 Name = StringVar()
-name_entry = Entry(obj, textvariable=Name , width=20, font ="arial 10")
-name_entry.place(x=160,y=50)
+name_entry = Entry(obj, textvariable=Name, width=20, font="arial 10")
+name_entry.place(x=160, y=50)
 
 DOB = StringVar()
-dob_entry = Entry(obj, textvariable=DOB , width=20, font ="arial 10")
-dob_entry.place(x=160,y=100)
-
-
+dob_entry = Entry(obj, textvariable=DOB, width=20, font="arial 10")
+dob_entry.place(x=160, y=100)
 
 radio = IntVar()
-R1 = Radiobutton(obj , text="Male" ,variable=radio , value=1,bg= framebg , fg= framefg ,command=selection)
-R1.place(x=150,y=150)
+R1 = Radiobutton(obj, text="Male", variable=radio, value=1, bg=FRAME_BG, fg=FRAME_FG, command=selection)
+R1.place(x=150, y=150)
 
-R2 = Radiobutton(obj , text="Female" ,variable=radio , value=2, bg= framebg , fg= framefg ,command=selection )
-R2.place(x=200,y=150)
+R2 = Radiobutton(obj, text="Female", variable=radio, value=2, bg=FRAME_BG, fg=FRAME_FG, command=selection)
+R2.place(x=200, y=150)
 
 Religion = StringVar()
-religion_entry = Entry(obj, textvariable=Religion , width=20, font ="arial 10")
-religion_entry.place(x=630,y=100)
+religion_entry = Entry(obj, textvariable=Religion, width=20, font="arial 10")
+religion_entry.place(x=630, y=100)
 
 Skill = StringVar()
-skill_entry = Entry(obj, textvariable=Skill , width=20, font ="arial 10")
-skill_entry.place(x=630,y=150)
+skill_entry = Entry(obj, textvariable=Skill, width=20, font="arial 10")
+skill_entry.place(x=630, y=150)
 
-Class = Combobox(obj , values=['1','2','3','4','5','6','7','8','9','10','11','12'] , font="Roboto 10" , width=17 , state="r")
-Class.place(x=630,y=50)
+Class = Combobox(obj, values=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], font="Roboto 10",
+                 width=17, state="r")
+Class.place(x=630, y=50)
 Class.set("Select Class")
 
-
-
 # Parent Details
-obj1 = LabelFrame(root , text= "Parent's Detail :" ,font = 20 , bd=2 , width= 900 , bg= framebg , fg= framefg,height=220 , relief=GROOVE )
-obj1.place(x=30 , y= 470)
+obj1 = LabelFrame(root, text="Parent's Detail :", font=20, bd=2, width=900, bg=FRAME_BG, fg=FRAME_FG, height=220,
+                  relief=GROOVE)
+obj1.place(x=30, y=470)
 
-Label(obj1, text= "Father's Name :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=30, y=50)
-Label(obj1, text= "Occupation :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=30, y=100)
+Label(obj1, text="Father's Name :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=30, y=50)
+Label(obj1, text="Occupation :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=30, y=100)
 
 F_Name = StringVar()
-f_entry = Entry(obj1, textvariable=F_Name , width=20, font ="arial 10")
-f_entry.place(x=160,y=50)
+f_entry = Entry(obj1, textvariable=F_Name, width=20, font="arial 10")
+f_entry.place(x=160, y=50)
 
 Father_Occupation = StringVar()
-fo_entry = Entry(obj1, textvariable=Father_Occupation , width=20, font ="arial 10")
-fo_entry.place(x=160,y=100)
+fo_entry = Entry(obj1, textvariable=Father_Occupation, width=20, font="arial 10")
+fo_entry.place(x=160, y=100)
 
-
-Label(obj1, text= "Mother's Name :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=500, y=50)
-Label(obj1, text= "Occupation :" , font="arial 13" , bg= framebg , fg= framefg ).place(x=500, y=100)
+Label(obj1, text="Mother's Name :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=500, y=50)
+Label(obj1, text="Occupation :", font="arial 13", bg=FRAME_BG, fg=FRAME_FG).place(x=500, y=100)
 
 M_Name = StringVar()
-m_entry = Entry(obj1, textvariable=M_Name , width=20, font ="arial 10")
-m_entry.place(x=630,y=50)
+m_entry = Entry(obj1, textvariable=M_Name, width=20, font="arial 10")
+m_entry.place(x=630, y=50)
 
 Mother_Occupation = StringVar()
-mo_entry = Entry(obj1, textvariable=Mother_Occupation , width=20, font ="arial 10")
-mo_entry.place(x=630,y=100)
+mo_entry = Entry(obj1, textvariable=Mother_Occupation, width=20, font="arial 10")
+mo_entry.place(x=630, y=100)
 
 #image
-f=Frame(root,bd=3,bg="black", width=200,height=200,relief=GROOVE)
-f.place(x=1000,y=150)
+f = Frame(root, bd=3, bg="black", width=200, height=200, relief=GROOVE)
+f.place(x=1000, y=150)
 
 img1 = PhotoImage(file="")
-lbl = Label(f,bg="black",image= img1)
-lbl.place(x=0,y=0)
+lbl = Label(f, bg="black", image=img1)
+lbl.place(x=0, y=0)
 
 #buttons
-Button(root,text="Upload",width=19,height=2,font="arial 12 bold" ,bg="lightblue", command=showimage).place(x=1000,y=370)
-savebutton = Button(root,text="Save",width=19,height=2,font="arial 12 bold" ,bg="lightgreen" , command=Save)
-savebutton.place(x=1000,y=450)
-Button(root,text="Reset",width=19,height=2,font="arial 12 bold" ,bg="lightpink" , command=Clear ).place(x=1000,y=530)
-Button(root,text="Exit",width=19,height=2,font="arial 12 bold" ,bg="grey",command=Exit).place(x=1000,y=610)
-
-
-
-
-
-
+Button(root, text="Upload", width=19, height=2, font="arial 12 bold", bg="lightblue", command=showimage).place(x=1000,
+                                                                                                               y=370)
+savebutton = Button(root, text="Save", width=19, height=2, font="arial 12 bold", bg="lightgreen", command=Save)
+savebutton.place(x=1000, y=450)
+Button(root, text="Reset", width=19, height=2, font="arial 12 bold", bg="lightpink", command=Clear).place(x=1000, y=530)
+Button(root, text="Exit", width=19, height=2, font="arial 12 bold", bg="grey", command=Exit).place(x=1000, y=610)
 
 root.mainloop()
